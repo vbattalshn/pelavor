490d57c7fe3825e49f4ad0c59643310296bda3b7
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2024-09-11 03:10:11 +0300

    Update [...slug].js

--- src/pages/list/[...slug].js
+++ src/pages/list/[...slug].js
@@ -114,7 +114,7 @@ function ListHead({
   url,
   joined,
 }) {
-  const [isJoined, setIsJoined] = useState(joined);
+  const [isJoined, setIsJoined] = useState(false);
 
   function JoinTheList() {
     const data = {
@@ -125,7 +125,7 @@ function ListHead({
       .post("/join-list", data)
       .then((response) => {
         toast.success(response.data.message);
-        setIsJoined(true);
+        setIsJoined(false);
       })
       .catch((error) => {
         toast.error(error.response?.data?.message || "Bir hata oluştu");
@@ -167,6 +167,12 @@ function ListHead({
           </div>
           <p>{description}</p>
           <div className="flex gap-2">
+                <Link
+                  href={"/play/" + url}
+                  className="px-4 py-2 rounded-full font-medium text-indigo-600 border border-indigo-600 active:scale-95 hocus:ring hocus:ring-indigo-600/50 hocus:bg-indigo-600 hocus:text-neutral-200 transition-all"
+                >
+                  Başla
+                </Link>
             {isJoined ? (
               <>
                 <Link
