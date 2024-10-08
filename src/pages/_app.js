d148a77f219ecfe6f7a6f6ab9d842a58e1b87469
Battal Şahin <battalsahin6@gmail.com>
2024-10-08 15:39:04 +0300

    v 1.4.5

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,9 @@ import apiClient from "@/lib/api";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import NextTopLoader from 'nextjs-toploader';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { useEffect } from "react/cjs/react.production.min";
 function MyApp({ Component, pageProps }) {
   apiClient.interceptors.response.use(
     (response) => {
@@ -24,8 +26,27 @@ function MyApp({ Component, pageProps }) {
     }
   );
 
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCc8lh1cg1pE6mgvCRPGpJKuVpAO9CMIkg",
+      authDomain: "pelavor-3272c.firebaseapp.com",
+      projectId: "pelavor-3272c",
+      storageBucket: "pelavor-3272c.appspot.com",
+      messagingSenderId: "144599565473",
+      appId: "1:144599565473:web:479544f1e3a0350b859ce0",
+      measurementId: "G-TQRV0HX4BF"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);    
+  })
+  
+
   return (
     <>
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-1113432847008327" />
+      </Head>
       <NextTopLoader color="#4f46e5" />
       <BugReporter />
       <Component {...pageProps} />
