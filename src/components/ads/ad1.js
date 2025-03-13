df2d8a7d9a918c7c77723d0c375e80a4c0cd9e83
Battal Şahin <73367836+vbattalshn@users.noreply.github.com>
2025-03-13 13:27:46 +0300

    Update ad1.js

--- src/components/ads/ad1.js
+++ src/components/ads/ad1.js
@@ -2,6 +2,7 @@ import Logo from "@/assets/logo";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
+import Script from 'next/script';
 
 export default function ad1() {
   const [premium, setPremium] = useState(false);
